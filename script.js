e6f0a069bf452c448d98a7a2c8183d6af92dd0e6
Batstone <adam.batstone@gmail.com>
2021-05-20 13:54:49 -0500

    push before hosting

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 $(function () {
 
+    // Animate on scroll initialization
+    AOS.init();
+
+    // adding click functionality to hamburger menu
+    $('.header__mobile-icon-container').on('click', () => {
+        $('.js-menu')
+            .toggleClass('header__mobile-icon')
+            .toggleClass('header__mobile-icon--active')
+
+        if ($('.header__list').css('display') === 'none') {
+            $('.header__list').css('display', 'flex')
+        } else {
+            $('.header__list').css('display', 'none')
+        };
+    });
+
+    // adding click functionality to hamburger menu
+    $('.header__main-mobile-icon-container').on('click', () => {
+        $('.js-menu')
+            .toggleClass('header__main-mobile-icon')
+            .toggleClass('header__main-mobile-icon--active')
+
+        if ($('.header__list').css('display') === 'none') {
+            $('.header__list').css('display', 'flex')
+        } else {
+            $('.header__list').css('display', 'none')
+        };
+    });
+
+    function placeOnLeftHandEdgeOfElementLine(toolbar, pageElement) {
+        $(toolbar).css("left", $(window).scrollLeft() + ($(window).width() + -180)
+            - $(pageElement).offset().left
+            - parseInt($(pageElement).css("borderLeftWidth"), 10)
+            - $(pageElement).width() + "px");
+    }
+
+    function placeOnLeftHandEdgeOfElement(toolbar, pageElement) {
+        $(toolbar).css("left", $(window).scrollLeft() + ($(window).width() + 50)
+            - $(pageElement).offset().left
+            - parseInt($(pageElement).css("borderLeftWidth"), 10)
+            - $(pageElement).width() + "px");
+    }
+
     function placeOnRightHandEdgeOfElement(toolbar, pageElement) {
         $(toolbar).css("right", $(window).scrollLeft() + ($(window).width() + -27)
             - $(pageElement).offset().left
@@ -11,4 +54,12 @@ $(function () {
     });
 
     $(".header__social-list").resize();
+
+
+    $(".main-header").ripples({
+        resolution: 512,
+        dropRadius: 30,
+        interactive: true,
+        perturbance: 0.005,
+    });
 });
